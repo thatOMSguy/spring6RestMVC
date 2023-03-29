8012b536b4ecd9f06aecbec484597a1e6def4548
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-03-29 11:04:06 +0530

    Beer POJO added with help of lombok

---
+++ src/main/java/com/springrestmvcproject/spring6restmvc/model/Beer.java
@@ -0,0 +1,23 @@
+package com.springrestmvcproject.spring6restmvc.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+
+@Data
+public class Beer {
+
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createDate;
+    private LocalDateTime updatedDate;
+}
---
+++ src/main/java/com/springrestmvcproject/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,4 @@
+package com.springrestmvcproject.spring6restmvc.model;
+
+public class BeerStyle {
+}
