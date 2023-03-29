1c6ca8fcb992b37058bcafea2a4385f59a689dfb
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-03-29 13:02:09 +0530

    Path parameter use for get beer by ID

--- src/main/java/com/springrestmvcproject/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/springrestmvcproject/spring6restmvc/controller/BeerController.java
@@ -6,7 +6,9 @@ import com.springrestmvcproject.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,23 +17,25 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
 
     private final BeerService beerService;
 
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
 
         return beerService.listBeers();
 
     }
 
-    public Beer getBearById(UUID id) {
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBearById(@PathVariable("beerId") UUID beerId) {
 
         log.debug("Get Beer By Id : Inside Controller");
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
 
 
     }
